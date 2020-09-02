856373f8b0efe2227e695bb6b30cac1e1671ad2b
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-09-02 12:14:57 +0200

    streamingish implementation

--- src/isal/isal_zlib.pyx
+++ src/isal/isal_zlib.pyx
@@ -84,28 +84,39 @@ cpdef compress(data, int level=ISAL_DEFAULT_COMPRESSION):
 
     cdef Py_ssize_t ibuflen = DEF_BUF_SIZE
     cdef Py_ssize_t obuflen = DEF_BUF_SIZE
-    cdef int err, flush
+    cdef Py_ssize_t tot_length = len(data)
+    cdef Py_ssize_t start, stop
     cdef isal_zstream stream
-    cdef bytes ibuf = data[0:ibuflen]
+
+    cdef bytes ibuf
     cdef bytearray obuf = bytearray(obuflen)
-
-
-    level_buf_size = zlib_mem_level_to_isal(level, DEF_MEM_LEVEL)
-    isal_deflate_init(&stream)
-    stream.next_in = ibuf
-    stream.avail_in = len(ibuf)
-    stream.next_out = obuf
-    stream.avail_out = obuflen
-    stream.level = level
+    cdef long level_buf_size = zlib_mem_level_to_isal(level, DEF_MEM_LEVEL)
     cdef bytearray level_buf = bytearray(level_buf_size)
+    cdef list out = []
+    cdef bint end_of_stream
+    cdef Py_ssize_t now_total_out, prev_total_out
+    isal_deflate_init(&stream)
+    stream.level = level
     stream.level_buf = level_buf
     stream.level_buf_size = level_buf_size
-    stream.flush = ISAL_FULL_FLUSH
+    stream.next_out = obuf
+    stream.avail_out = obuflen
     stream.gzip_flag = IGZIP_ZLIB
-    stream.end_of_stream = 1
-    ret = isal_deflate(&stream)
-    check_isal_deflate_rc(ret)
-    return bytes(obuf[:stream.total_out])
+    now_total_out = stream.total_out
+    for start in range(0, tot_length, ibuflen):
+        stop = start+ibuflen
+        ibuf = data[start: stop]
+        stream.next_in = ibuf
+        stream.avail_in = len(ibuf)
+        end_of_stream = stop >= tot_length
+        stream.flush = ISAL_FULL_FLUSH if end_of_stream else ISAL_NO_FLUSH
+        stream.end_of_stream = end_of_stream
+        prev_total_out = now_total_out
+        ret = isal_deflate(&stream)
+        check_isal_deflate_rc(ret)
+        now_total_out = stream.total_out
+        out.append(bytes(obuf[:now_total_out - prev_total_out]))
+    return b"".join(out)
 
 cpdef compressobj(int level=ISAL_DEFAULT_COMPRESSION,
                   int method=zlib.DEFLATED,
