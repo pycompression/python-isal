06db071d41ca611b02749f62062960441638fc64
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-09-03 09:58:13 +0200

    Add decompress tests

--- tests/test_isal_zlib.py
+++ tests/test_isal_zlib.py
@@ -18,37 +18,57 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from pathlib import Path
 import itertools
 import zlib
+from pathlib import Path
 
 from isal import isal_zlib
 
 import pytest
 
 DATA_DIR = Path(__file__).parent / "data"
-DATA_FILE =  DATA_DIR / "512kb.txt"
+DATA_FILE = DATA_DIR / "512kb.txt"
 DATA_512KB = DATA_FILE.read_bytes()
-DATA_SIZES = [2**i for i in range(3,20)]
+DATA_SIZES = [2**i for i in range(3, 20)]
 # 100 seeds generated with random.randint(0, 2**32-1)
 SEEDS_FILE = DATA_DIR / "seeds.txt"
 # Create seeds 0, 1 and 20 seeds from the seeds file.
 SEEDS = [0, 1] + [int(seed) for seed in SEEDS_FILE.read_text().splitlines()]
 
 
-@pytest.mark.parametrize(["data_size", "value"], itertools.product(DATA_SIZES, SEEDS))
+@pytest.mark.parametrize(["data_size", "value"],
+                         itertools.product(DATA_SIZES, SEEDS))
 def test_crc32(data_size, value):
     data = DATA_512KB[:data_size]
     assert zlib.crc32(data, value) == isal_zlib.crc32(data, value)
 
 
-@pytest.mark.parametrize(["data_size", "value"], itertools.product(DATA_SIZES, SEEDS))
+@pytest.mark.parametrize(["data_size", "value"],
+                         itertools.product(DATA_SIZES, SEEDS))
 def test_adler32(data_size, value):
     data = DATA_512KB[:data_size]
     assert zlib.adler32(data, value) == isal_zlib.adler32(data, value)
 
-@pytest.mark.parametrize(["data_size", "level"], itertools.product(DATA_SIZES, range(4)))
+
+@pytest.mark.parametrize(["data_size", "level"],
+                         itertools.product(DATA_SIZES, range(4)))
 def test_compress(data_size, level):
     data = DATA_512KB[:data_size]
     compressed = isal_zlib.compress(data, level=level)
     assert zlib.decompress(compressed) == data
+
+
+@pytest.mark.parametrize(["data_size", "level"],
+                         itertools.product(DATA_SIZES, range(10)))
+def test_decompress_zlib(data_size, level):
+    data = DATA_512KB[:data_size]
+    compressed = zlib.compress(data, level=level)
+    assert isal_zlib.decompress(compressed) == data
+
+
+@pytest.mark.parametrize(["data_size", "level"],
+                         itertools.product(DATA_SIZES, range(4)))
+def test_decompress_isal_zlib(data_size, level):
+    data = DATA_512KB[:data_size]
+    compressed = isal_zlib.compress(data, level=level)
+    assert isal_zlib.decompress(compressed) == data
