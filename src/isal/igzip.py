02f188e6be3cbff71a9d02b5932e5f8fe95d60b5
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-09-28 16:05:51 +0200

    Fix small lint error

--- src/isal/igzip.py
+++ src/isal/igzip.py
@@ -97,8 +97,8 @@ class IGzipFile(gzip.GzipFile):
     """The IGzipFile class simulates most of the methods of a file object with
     the exception of the truncate() method.
 
-    This class only supports opening files in binary mode. If you need to open a
-    compressed file in text mode, use the gzip.open() function.
+    This class only supports opening files in binary mode. If you need to open
+    a compressed file in text mode, use the gzip.open() function.
     """
     def __init__(self, filename=None, mode=None,
                  compresslevel=isal_zlib.ISAL_DEFAULT_COMPRESSION,
