8f4bae5514d9c22bd0598c1da6e8e8dabc3c0dde
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2021-07-23 09:48:41 +0200

    Update version

--- setup.py
+++ setup.py
@@ -170,7 +170,7 @@ def build_isa_l(compiler_command: str, compiler_options: str):
 
 setup(
     name="isal",
-    version="0.11.0",
+    version="1.0.0-dev",
     description="Faster zlib and gzip compatible compression and "
                 "decompression by providing python bindings for the ISA-L "
                 "library.",
