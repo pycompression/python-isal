4e7dc725f0fa76a89867faa214f32eddcb82b5c9
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2021-10-11 11:52:32 +0200

    Remove development status to stable

--- setup.py
+++ setup.py
@@ -206,7 +206,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Cython",
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Topic :: System :: Archiving :: Compression",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
