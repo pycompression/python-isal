8b7ea5e252dd9d5f3d7db98ffa76b9bbab70aae6
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-01-07 08:50:09 +0100

    Do not use generic new

--- src/isal/igzip_lib.c
+++ src/isal/igzip_lib.c
@@ -712,7 +712,7 @@ static PyTypeObject IgzipDecompressor_Type = {
     0,                                  /* tp_dictoffset */
     igzip_lib_IgzipDecompressor___init__,      /* tp_init */
     0,                                  /* tp_alloc */
-    PyType_GenericNew,                  /* tp_new */
+    0,                                  /* tp_new */
 };
 
 static PyMethodDef IgzipLibMethods[] = {
