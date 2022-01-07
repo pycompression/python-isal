ea054119d50386543324e9ac9eae49386fec6aee
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-01-07 08:59:43 +0100

    Fix trailing whitespace

--- src/isal/igzip.py
+++ src/isal/igzip.py
@@ -2,7 +2,7 @@
 # 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022
 # Python Software Foundation; All Rights Reserved
 
-# This file is part of python-isal which is distributed under the 
+# This file is part of python-isal which is distributed under the
 # PYTHON SOFTWARE FOUNDATION LICENSE VERSION 2.
 
 # This file uses code from CPython's Lib/gzip.py
