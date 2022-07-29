cfcbce79170f18089907f8b1691bc11909a33b2b
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-07-29 08:46:23 +0200

    Simplify ISAL_VERSION creation

--- src/isal/_isalmodule.c
+++ src/isal/_isalmodule.c
@@ -11,7 +11,6 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include <isa-l.h>
-#include <stdio.h>
 
 static struct PyModuleDef _isal_module = {
     PyModuleDef_HEAD_INIT,
@@ -34,19 +33,9 @@ PyInit__isal(void)
     PyModule_AddIntMacro(m, ISAL_MAJOR_VERSION);
     PyModule_AddIntMacro(m, ISAL_MINOR_VERSION);
     PyModule_AddIntMacro(m, ISAL_PATCH_VERSION);
-    // UINT64_MAX is 20 characters long. A version contains:
-    // 3 numbers, 2 dots, one null byte. So the maximum size
-    // in charcters is 3x20+2+1=63. Round up to the nearest 
-    // power of 2 is 64.
-    char version[64];
-    int length = snprintf(version, 64, "%d.%d.%d",
-                          ISAL_MAJOR_VERSION, ISAL_MINOR_VERSION, ISAL_PATCH_VERSION);  
-    if (length > 64){
-        // This is extremely unlikely to happen given the calculation above.
-        PyErr_SetString(PyExc_MemoryError, "Could not allocate enough memory for ISA-L version string");
-        return NULL;
-    }
-    PyObject *isal_version = PyUnicode_DecodeASCII(version, length, "strict");
+
+    PyObject *isal_version = PyUnicode_FromFormat(
+        "%d.%d.%d", ISAL_MAJOR_VERSION, ISAL_MINOR_VERSION, ISAL_PATCH_VERSION);  
     if (isal_version == NULL)
         return NULL;
     PyModule_AddObject(m, "ISAL_VERSION", isal_version);
