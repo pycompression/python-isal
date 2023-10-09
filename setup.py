15a4810cd2073548649be94bd70c6c13ebb1cf12
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-10-09 07:30:05 +0200

    Set patch version

--- setup.py
+++ setup.py
@@ -135,7 +135,7 @@ def build_isa_l():
 
 setup(
     name="isal",
-    version="1.5.0-dev",
+    version="1.4.1",
     description="Faster zlib and gzip compatible compression and "
                 "decompression by providing python bindings for the ISA-L "
                 "library.",
