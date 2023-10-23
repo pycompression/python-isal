2f1fa238a4a09da8cb74990d224848b2150607af
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-10-23 10:29:05 +0200

    Also test memory usage for decompress function

--- benchmark_scripts/memory_leak_test.py
+++ benchmark_scripts/memory_leak_test.py
@@ -1,7 +1,19 @@
 import sys
 from isal import igzip
+from pathlib import Path
 import resource
 import gc
+
+README = Path(__file__).parent.parent / "README.rst"
+GZIPPED_DATA = igzip.compress(README.read_bytes())
+
+for _ in range(10):
+    for _ in range(1000):
+        a = igzip.decompress(GZIPPED_DATA)
+    memory_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+    memory_usage_mb = memory_usage / 1024
+    print(f"Maximum memory usage: {memory_usage_mb:.2f} MiB")
+
 
 for _ in range(10):
     with igzip.open(sys.argv[1], "rb") as reader:
@@ -12,6 +24,8 @@ for _ in range(10):
         memory_usage_mb = memory_usage / 1024
         print(f"Maximum memory usage: {memory_usage_mb:.2f} MiB")
         del(a)
+
+
 objects_and_size = [(sys.getsizeof(obj), type(obj)) for obj in
                     gc.get_objects()]
 objects_and_size.sort(key=lambda x: x[0], reverse=True)
