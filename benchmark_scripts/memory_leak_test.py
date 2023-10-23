817d4181b39d4e9f3de4c7031e9e909d963baedd
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-10-23 10:44:06 +0200

    Fix linting errors in benchmark script

--- benchmark_scripts/memory_leak_test.py
+++ benchmark_scripts/memory_leak_test.py
@@ -1,7 +1,8 @@
-import sys
-from isal import igzip
-import resource
 import gc
+import resource
+import sys
+
+from isal import igzip
 
 for _ in range(10):
     with igzip.open(sys.argv[1], "rb") as reader:
@@ -11,7 +12,7 @@ for _ in range(10):
         memory_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
         memory_usage_mb = memory_usage / 1024
         print(f"Maximum memory usage: {memory_usage_mb:.2f} MiB")
-        del(a)
+        del a
 objects_and_size = [(sys.getsizeof(obj), type(obj)) for obj in
                     gc.get_objects()]
 objects_and_size.sort(key=lambda x: x[0], reverse=True)
